a7d3a38ab8dfd6a649a0475a0516154300399b8a
KazakovaNet <kazakovaNet@yandex.ru>
2020-01-04 00:50:03 +0300

    Update gradle and libraries versions

--- app/src/main/java/udacity/pokemon/MainActivity.java
+++ app/src/main/java/udacity/pokemon/MainActivity.java
@@ -15,9 +15,8 @@
  */
 package udacity.pokemon;
 
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListAdapter;
